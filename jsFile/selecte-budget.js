b2170584e7822e191d4535dc41037d87f5f08706
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2022-08-23 01:06:46 +0600

    selected part added

--- jsFile/selecte-budget.js
+++ jsFile/selecte-budget.js
@@ -1,46 +1,26 @@
-// function handelOnClick (event) {
-//    const clickOnFirstButton = document.getElementById('first-btn')
-//     // console.log("select btn are clicked");
-   
-//     const playersName = document.getElementById(event);
-//     const player1Name = playersName.innerText;
-//     // playersName.innerText = player1Name; 
-//     console.log(player1Name);
-
-//     const clickOnScondButton = document.getElementById ('second-btn');
-   
-//     const player2Name = playersName.innerText;
-//     console.log(player2Name);
-
-   
-// }
+const ol = document.getElementById('list-container');
+const players = document.getElementsByClassName('player');
 
 function handelOnClick (event) {
-    const playerNameList = document.getElementById (event);
-    const fistPlayerNameAdd = playerNameList.innerText;
-
-    const secondPlayerNameAdd = playerNameList.innerText;
-
-    const thirdPlayerNameAdd = playerNameList.innerText;
-
-    const fourthPlayerNameAdd = playerNameList.innerText;
-
-    const fivethPlayerNameAdd = playerNameList.innerText;
-
-    const sixPlayerNameAdd = playerNameList.innerText;
-
-    const listContainer = document.getElementById('list-container');
-        const li = document.createElement('li');
-        li.innerText = fistPlayerNameAdd, secondPlayerNameAdd, thirdPlayerNameAdd, fourthPlayerNameAdd, fivethPlayerNameAdd, sixPlayerNameAdd;
-        const allLiList = li.innerText;
-        // li.innerText = secondPlayerNameAdd;
-        // li.innerText = thirdPlayerNameAdd;
-        // li.innerText = fourthPlayerNameAdd;
-        // li.innerText = fivethPlayerNameAdd;
-        // li.innerText = sixPlayerNameAdd;
-        listContainer.appendChild(li);
-        if(allLiList.length >= 5){
-            alert("You can't add more than 5 Players");
-            }
-    
+   const clicksOnButton = document.getElementsByClassName('btn-style');
+    for(const clickButton of clicksOnButton) {
+        clickButton.addEventListener('click', function(){
+            clickButton.setAttribute('disabled', true)
+        })
+    }
+   
+    const playersName = document.getElementById(event);
+    const playerName = playersName.innerText;
+    const li = document.createElement('li') ;
+    li.innerText = playerName;
+    li.classList = 'player';
+    if (players.length >= 5) {
+        alert('you can not more than 5');
+        return;
+    }
+    else{
+        ol.appendChild(li);
+    }
+   
 }
+
